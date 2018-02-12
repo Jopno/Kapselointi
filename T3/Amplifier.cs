eadaf318c1017b4731cbe4fd6b45163c9ef0ac2e
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-12 20:21:24 +0200

    Add files via upload

---
+++ T3/Amplifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmplifierApplication
+{
+    class Amplifier
+    {
+
+        private int volume;
+
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value  <= 100 && value >= 0)
+                {
+                    volume = value;
+                    Console.WriteLine("Amplifier volume is set to : {0} " , volume);
+                }
+                else if  (value > 100)
+                {
+                    Console.WriteLine("Volume is too loud - Volume is set to maximum : 100");
+                }
+
+                else if (value < 0)
+                {
+                    Console.WriteLine("Volume is too low - Volume is set to minimum : 0");
+                }
+            }
+        }
+    }
+}
---
+++ T3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmplifierApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Amplifier amp = new Amplifier();
+            while (true)
+            {
+                Console.Write("Give a new volume value (0-100) > ");
+                bool result = Int32.TryParse(Console.ReadLine(), out number);
+                if (result)
+                    amp.Volume = number;
+                else
+                {
+                    Console.WriteLine("Not a number!");
+                    continue;
+                }
+            }
+        }
+    }
+}
